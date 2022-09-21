148a0444a575c842a2f280861283abb915ec579f
ParthGurjar2000 <114073729+ParthGurjar2000@users.noreply.github.com>
2022-09-21 17:44:27 +0530

    Add files via upload

---
+++ Lab Assignment/test/Ui/DisplayJPanelTest.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package ui;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Parth Gurjar
+ */
+public class DisplayJPanelTest {
+    
+    public DisplayJPanelTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    /**
+     * Test of displayProduct method, of class DisplayJPanel.
+     */
+    @Test
+    public void testDisplayProduct() {
+        System.out.println("displayProduct");
+        /*DisplayJPanel*/ instance = new DisplayJPanel();
+        instance.displayProduct();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
