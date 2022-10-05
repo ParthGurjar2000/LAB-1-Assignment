d57fa5eced9c75fbe7fc58e24a919577803ec4f2
ParthGurjar2000 <114073729+ParthGurjar2000@users.noreply.github.com>
2022-10-05 06:59:06 +0530

    Add files via upload

---
+++ Lab Assignment 2/src/model/EmployeeHistory.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.io.File;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Parth Gurjar
+ */
+public class EmployeeHistory {
+    
+    private ArrayList<Employee> history;
+    
+    public EmployeeHistory(){
+        
+        this.history = new ArrayList<Employee>();
+    }
+
+    public ArrayList<Employee> getHistory() {
+        return history;
+    }
+
+    public void setHistory(ArrayList<Employee> history) {
+        this.history = history;
+    }
+
+    public Employee addNewEmployee() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public void setPhoto(File f) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
